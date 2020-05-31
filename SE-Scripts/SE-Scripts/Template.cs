f54e5a4962720ee423ff0271f7cd5b358e9ea286
Anders Nielsen <Anders041292@hotmail.com>
2020-05-31 14:33:14 +0200

    Second Setup

---
+++ SE-Scripts/SE-Scripts/Template.cs
@@ -0,0 +1,67 @@
+﻿#region Prelude
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using VRageMath;
+using VRage.Game;
+using VRage.Collections;
+using Sandbox.ModAPI.Ingame;
+using VRage.Game.Components;
+using VRage.Game.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using Sandbox.Game.EntityComponents;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using VRage.Game.ObjectBuilders.Definitions;
+
+
+// Change this namespace for each script you create.
+namespace SpaceEngineers.UWBlockPrograms.Template
+{
+    public sealed class Template : MyGridProgram
+    {
+        #endregion
+        // ------- Code Below --------
+
+        public void Program()
+        {
+            //!--- Remember to remove 'void'. Its here just to avoid errors, but in SE the Program() constructor should not have a return type.
+
+            // The constructor, called only once every session and
+            // always before any other method is called. Use it to
+            // initialize your script. 
+            //     
+            // The constructor is optional and can be removed if not
+            // needed.
+            // 
+            // It's recommended to set RuntimeInfo.UpdateFrequency 
+            // here, which will allow your script to run itself without a 
+            // timer block.
+        }
+
+        public void Save()
+        {
+            // Called when the program needs to save its state. Use
+            // this method to save your state to the Storage field
+            // or some other means. 
+            // 
+            // This method is optional and can be removed if not
+            // needed.
+        }
+
+        public void Main(string argument, UpdateType updateSource)
+        {
+            // The main entry point of the script, invoked every time
+            // one of the programmable block's Run actions are invoked,
+            // or the script updates itself. The updateSource argument
+            // describes where the update came from.
+            // 
+            // The method itself is required, but the arguments above
+            // can be removed if not needed.
+        }
+
+        // ------ Code Above ----------
+        #region PreludeFooter
+    }
+}
+#endregion
